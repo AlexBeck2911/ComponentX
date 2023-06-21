d0569aac586beda9b1a339fa660c5feb74000c70
AlexanderAdBeck <Alexander.Ad.Beck@deutschebahn.com>
2023-06-21 12:04:04 +0200

    docs

--- components/vue/index.ts
+++ components/vue/index.ts
@@ -1,4 +1,4 @@
-import RadioGroupComponent from "../../output/vue/vue3/src/components/radio/radio-group-component.vue";
-import RadioOptionComponent from "../../output/vue/vue3/src/components/radio/radio-option-component.vue";
+import RadioGroup from "../../output/vue/vue3/src/components/radio/radio-group-component.vue";
+import RadioOption from "../../output/vue/vue3/src/components/radio/radio-option-component.vue";
 
-export default {RadioGroupComponent, RadioOptionComponent};
+export default {RadioGroup, RadioOption};
